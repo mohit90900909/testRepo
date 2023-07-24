94a8cb9b45ef22d469cfad1f390e25ae591acf0b
mohit90900909 <60419994+mohit90900909@users.noreply.github.com>
2023-07-24 16:18:15 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class hello
 public static void main(String args[]){
 System.out.println("Hi, sapient");
 System.out.println(3+4);
-  System.out.println("kaise bataye ki tujhko chahein");
+  System.out.println("this is adding another line");
 }
 }
